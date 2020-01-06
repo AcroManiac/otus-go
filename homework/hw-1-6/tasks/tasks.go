fe144e230a019f4711dc5ec491b1c27e52efb076
Artem Orlov <a.orlov@veedo.ru>
2020-01-06 20:45:39 +0300

    Made wait group syncing on channels to prevent writing on closed channel

--- homework/hw-1-6/tasks/tasks.go
+++ homework/hw-1-6/tasks/tasks.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"runtime"
+	"sync"
 )
 
 func Run(tasks []func() error, N int, M int) error {
@@ -12,18 +13,27 @@ func Run(tasks []func() error, N int, M int) error {
 	runtime.GOMAXPROCS(N)
 
 	// Create working channels
-	errChan := make(chan error, M)   // buffered error channel
-	doneChan := make(chan string, N) // buffered success channel
+	// (make channels buffered to prevent goroutines leaking)
+	errChan := make(chan error, M)   // error channel
+	doneChan := make(chan string, N) // success channel
+
+	// Use wait group to synchronize writes to channels on function exit
+	var wg sync.WaitGroup
 
 	defer func() {
+		wg.Wait() // Wait until all tasks stop writing to channels
 		close(errChan)
 		close(doneChan)
 		log.Println("Run is exited")
 	}()
 
-	// Running tasks
+	// Running tasks in separate goroutines
 	for _, task := range tasks {
+		wg.Add(1) // increment wait group counter
 		go func(t func() error) {
+			defer wg.Done() // decrement wait group counter on goroutine exit
+
+			// Calling the working task
 			err := t()
 			if err != nil {
 				// Make error handling - send error in channel
@@ -52,11 +62,13 @@ func Run(tasks []func() error, N int, M int) error {
 				}
 			}
 		case err := <-errChan:
-			// Read errors from channel
+			// Checking errors from channel
 			if err != nil {
 				log.Printf("Task return error: %s", err.Error())
 				errCounter++
 				if errCounter == M {
+					// WARNING!!!!
+					// Go routines leaking!
 					return errors.New("error limit is elapsed")
 				}
 			}
