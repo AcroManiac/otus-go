7206b286f5a9f5af6ad68388a94f1c2b82e1e5a3
Artem Orlov <a.orlov@veedo.ru>
2020-01-07 09:43:53 +0300

    Added load unit test

--- homework/hw-1-6/tasks/tasks_test.go
+++ homework/hw-1-6/tasks/tasks_test.go
@@ -22,7 +22,7 @@ func emulateActivity(taskNumber, delaySec int, error bool) {
 	}
 }
 
-var tasksNoErrors = []func() error{
+var tasks1 = []func() error{
 	func() error {
 		emulateActivity(1, rand.Intn(5), false)
 		return nil
@@ -41,7 +41,7 @@ var tasksNoErrors = []func() error{
 	},
 }
 
-var tasksWithErrors = []func() error{
+var tasks2 = []func() error{
 	func() error {
 		emulateActivity(1, 1, true)
 		return errors.New("error in task 1")
@@ -64,6 +64,30 @@ var tasksWithErrors = []func() error{
 	},
 }
 
+var tasks3 []func() error
+
+func tasks3Builder(number int) {
+	for i := 0; i < number; i++ {
+		tasks3 = append(tasks3, func() error {
+
+			// Emulate activity
+			var delaySec = rand.Intn(5)
+			time.Sleep(time.Duration(delaySec) * time.Second)
+
+			// Emulate result || error
+			var err error
+			switch rand.Intn(2) {
+			case 0:
+				err = errors.New("error in task")
+			case 1:
+				log.Printf("Task fired in %d seconds", delaySec)
+				err = nil
+			}
+			return err
+		})
+	}
+}
+
 const (
 	concurrentTaskNumber = 4
 	errorsLimit          = 2
@@ -72,14 +96,22 @@ const (
 func TestRun(t *testing.T) {
 	var err error
 	log.Println("************ Test 1 ************")
-	if err = Run(tasksNoErrors, concurrentTaskNumber, errorsLimit); err != nil {
+	if err = Run(tasks1, concurrentTaskNumber, errorsLimit); err != nil {
 		log.Printf("An error occured: %s", err.Error())
 	}
 	assert.Nil(t, err, "There should be no errors in this test")
 
 	log.Println()
 	log.Println("************ Test 2 ************")
-	if err = Run(tasksWithErrors, concurrentTaskNumber, errorsLimit); err != nil {
+	if err = Run(tasks2, concurrentTaskNumber, errorsLimit); err != nil {
+		log.Printf("An error occured: %s", err.Error())
+	}
+	assert.NotNil(t, err, "There should be error in this test")
+
+	log.Println()
+	log.Println("************ Test 3 ************")
+	tasks3Builder(20)
+	if err = Run(tasks3, 10, 5); err != nil {
 		log.Printf("An error occured: %s", err.Error())
 	}
 	assert.NotNil(t, err, "There should be error in this test")
