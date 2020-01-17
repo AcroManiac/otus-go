5012a04f6327f0027f63c475e51022c27f399dd9
Artem Orlov <a.orlov@veedo.ru>
2020-01-17 18:15:10 +0300

    Added main func with parameters parsing

---
+++ homework/hw-2-1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/AcroManiac/otus-go/homework/hw-2-1/gocopy"
+)
+
+var (
+	flagTo     string
+	flagFrom   string
+	flagLimit  int
+	flagOffset int
+)
+
+func init() {
+	flag.StringVar(&flagTo, "to", "", "destination file")
+	flag.StringVar(&flagFrom, "from", "", "source file")
+	flag.IntVar(&flagLimit, "limit", -1, "copying bytes number limit")
+	flag.IntVar(&flagOffset, "offset", 0, "bytes offset from file start")
+}
+
+func main() {
+	flag.Parse()
+
+	if err := gocopy.Copy(flagFrom, flagTo, flagLimit, flagOffset); err != nil {
+		log.Fatalf("An error occurred while file copying: %s", err.Error())
+	}
+
+	log.Println("File copied successfully")
+}
