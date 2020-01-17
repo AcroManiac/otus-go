4e80742b3dfb756a7fa4e1bb14a390dacc8320e2
Artem Orlov <a.orlov@veedo.ru>
2020-01-17 17:10:35 +0300

    Added first unit test

--- homework/hw-2-1/gocopy/gocopy.go
+++ homework/hw-2-1/gocopy/gocopy.go
@@ -1,5 +1,61 @@
 package gocopy
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+const copySize = 50 * 1024 // 50 KiBytes
+
 func Copy(from string, to string, limit int, offset int) error {
-	//
+	// Open reader
+	file, err := os.Open(from)
+	if err != nil {
+		return fmt.Errorf("could not open file for reading: %s", err.Error())
+	}
+	defer file.Close()
+
+	// Evaluate bytes to write
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("could not get file info: %s", err.Error())
+	}
+	var bytesToWrite int64 = info.Size()
+
+	// Set limit of bytes to read
+	var input io.Reader = file
+	if limit != -1 {
+		input = io.LimitReader(file, int64(limit))
+		bytesToWrite = int64(limit)
+	}
+
+	// Set offset position for reading
+	if offset > 0 {
+		offset64 := int64(offset)
+		if pos, err := file.Seek(offset64, 0); err != nil || pos != offset64 {
+			return fmt.Errorf("could not set offset for reading: %s", err.Error())
+		}
+	}
+
+	// Create writer
+	output, err := os.Create(to)
+	if err != nil {
+		return fmt.Errorf("could not open file for writing: %s", err.Error())
+	}
+	defer output.Close()
+
+	// Copy data
+	for totalWritten := int64(0); totalWritten < bytesToWrite; {
+		written, err := io.CopyN(output, input, copySize)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return fmt.Errorf("error while copying the file: %s", err.Error())
+		}
+		totalWritten += written
+	}
+
+	return nil
 }
--- homework/hw-2-1/gocopy/gocopy_test.go
+++ homework/hw-2-1/gocopy/gocopy_test.go
@@ -1,7 +1,66 @@
 package gocopy
 
-import "testing"
+import (
+	"io"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const autogenSize = 1 * 1024 * 1024 // 1 MByte
+
+var copyTests = []struct {
+	outputFileName   string
+	limit            int
+	offset           int
+	expectedFileSize int64
+}{
+	{
+		"test-copy-1.dat",
+		-1,
+		0,
+		autogenSize,
+	},
+}
+
+func deleteFile(path string) {
+	if err := os.Remove(path); err != nil {
+		log.Fatalf("Error while deleting file %s: %s", path, err.Error())
+	}
+}
 
 func TestCopy(t *testing.T) {
-	//
+	var err error
+
+	// Create test file with random content
+	autogen, err := os.Create("test-autogen.dat")
+	if err != nil {
+		log.Fatalf("Could not open file for writing: %s", err.Error())
+	}
+	random, err := os.Open("/dev/urandom")
+	if err != nil {
+		log.Fatalf("Could not open random content generator: %s", err.Error())
+	}
+	written, err := io.CopyN(autogen, random, autogenSize)
+	if err != nil {
+		log.Fatalf("Could not create random content: %s", err.Error())
+	}
+	if written != autogenSize {
+		log.Fatalf("Number of written bytes is less than expected: %d", written)
+	}
+	autogen.Close()
+	random.Close()
+
+	for _, test := range copyTests {
+		err = Copy("test-autogen.dat", test.outputFileName, test.limit, test.offset)
+		assert.Nil(t, err, "There should be no errors in this test")
+
+		// Delete copy file
+		deleteFile(test.outputFileName)
+	}
+
+	// Delete test file with content
+	deleteFile("test-autogen.dat")
 }
