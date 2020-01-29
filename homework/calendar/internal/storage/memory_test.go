d3b3443f3f661c07ce57f53aa23ff6c6cccca42d
Artem Orlov <a.orlov@veedo.ru>
2020-01-29 12:47:19 +0300

    Added more unit tests

--- homework/calendar/internal/storage/memory_test.go
+++ homework/calendar/internal/storage/memory_test.go
@@ -1,10 +1,12 @@
 package storage
 
 import (
-	"github.com/AcroManiac/otus-go/homework/calendar/internal/event"
-	"github.com/google/uuid"
 	"testing"
 	"time"
+
+	"github.com/AcroManiac/otus-go/homework/calendar/internal/event"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 var location, _ = time.LoadLocation("Europe/Moscow")
@@ -59,17 +61,73 @@ func createMemoryStorage(t *testing.T) *MemoryStorage {
 	return ms
 }
 
-var addTests = struct {
-	expected []time.Time
-}{
-	[]time.Time{events[0].StartTime, events[1].StartTime, events[2].StartTime, events[3].StartTime},
-}
-
 func TestMemoryStorage_Add(t *testing.T) {
+	var addTests = struct {
+		expected []time.Time
+	}{
+		[]time.Time{events[0].StartTime, events[1].StartTime, events[2].StartTime, events[3].StartTime},
+	}
 	ms := createMemoryStorage(t)
 	for _, startTime := range addTests.expected {
 		if !ms.isExist(startTime) {
 			t.Errorf("event with time %v was not populated", startTime)
 		}
 	}
+
+	// Test error case
+	err := ms.Add(events[0])
+	assert.NotNil(t, err, "Method should return an error")
+}
+
+func TestMemoryStorage_Remove(t *testing.T) {
+	var removeTests = struct {
+		existed []time.Time
+		removed []time.Time
+	}{
+		[]time.Time{events[0].StartTime, events[2].StartTime},
+		[]time.Time{events[1].StartTime, events[3].StartTime},
+	}
+	ms := createMemoryStorage(t)
+
+	// Remove events from storage
+	for _, startTime := range removeTests.removed {
+		err := ms.Remove(startTime)
+		assert.Nil(t, err, "Method should return no error")
+
+		// Check if event was removed really
+		ok := ms.isExist(startTime)
+		assert.Falsef(t, ok, "event with time %v shouldn't exist in storage", startTime)
+	}
+
+	// Check remained events
+	for _, startTime := range removeTests.existed {
+		ok := ms.isExist(startTime)
+		assert.Truef(t, ok, "event with time %v should exist in storage", startTime)
+	}
+}
+
+func TestMemoryStorage_Edit(t *testing.T) {
+	ms := createMemoryStorage(t)
+
+	// Modify time for event in storage
+	me := events[3]
+	me.StartTime = events[0].StartTime.Add(time.Hour)
+	err := ms.Edit(events[3].StartTime, me)
+	assert.Nil(t, err, "Method should return no error")
+	me.StartTime = events[0].StartTime
+	err = ms.Edit(events[3].StartTime, me)
+	assert.NotNil(t, err, "Method should return error")
+}
+
+func TestMemoryStorage_GetByTimePeriod(t *testing.T) {
+	var getTests = struct {
+		expected []event.Event
+	}{
+		events[0:3],
+	}
+	ms := createMemoryStorage(t)
+
+	filtered, err := ms.GetByTimePeriod(events[0].StartTime, Week)
+	assert.Nil(t, err, "Method should return no error")
+	assert.ElementsMatch(t, getTests.expected, filtered, "Events should match")
 }
