450142a7b347fa73c60fe954cd042c95fef9f2d9
Artem Orlov <a.orlov@veedo.ru>
2020-02-02 12:09:16 +0300

    Added comments for calendar interface

--- homework/calendar/internal/calendar/calendar.go
+++ homework/calendar/internal/calendar/calendar.go
@@ -7,8 +7,19 @@ import (
 	"github.com/AcroManiac/otus-go/homework/calendar/internal/storage"
 )
 
+// Calendar interface
 type Calendar interface {
+
+	// CreateEvent function constructs new event with default values
+	// and adds it to event storage. Returns Id of generated event or
+	// storage error if any
 	CreateEvent(startTime time.Time, stopTime time.Time) (storage.EventId, error)
+
+	// EditEvent finds event in storage by Id and replaces it
+	// with event in parameters. Returns error if event Id is wrong
 	EditEvent(id storage.EventId, event event.Event) error
+
+	// DeleteEvent removes event from storage by Id.
+	// Returns error if event Id is wrong
 	DeleteEvent(id storage.EventId) error
 }
