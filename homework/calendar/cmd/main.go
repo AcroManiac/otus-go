66aaae75d4df06ca702815ff9dffeac1b24c66b0
Artem Orlov <a.orlov@veedo.ru>
2020-02-02 22:44:05 +0300

    Added configuring from file and logging

--- homework/calendar/cmd/main.go
+++ homework/calendar/cmd/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"time"
 
+	"github.com/AcroManiac/otus-go/homework/calendar/internal/logger"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+
 	"github.com/AcroManiac/otus-go/homework/calendar/internal/calendar"
 )
+
+func init() {
+	// using standard library "flag" package
+	flag.String("config", "../configs/calendar.yaml", "path to configuration flag")
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
+	// Reading configuration from file
+	configPath := viper.GetString("config") // retrieve value from viper
+	viper.SetConfigFile(configPath)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Couldn't read configuration file: %s", err.Error())
+	}
+
+	// Setting log parameters
+	logger.Init(viper.GetString("log.log_level"), viper.GetString("log.log_file"))
+}
 
 func main() {
 	// Create calendar
@@ -13,8 +37,10 @@ func main() {
 
 	// Create and add event
 	if _, err := cal.CreateEvent(time.Now(), time.Now().Add(time.Hour)); err != nil {
-		log.Printf("Error adding event: %s", err.Error())
+		logger.Error("Error adding event", "error", err.Error())
 	}
 
-	log.Println("Calendar was created. Bye!")
+	logger.Info("Calendar was created. Bye!")
+
+	//log.Println(viper.GetString("http_listen.ip"))
 }
