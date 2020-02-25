6124ac21831a59f62ca12feffce3ba36ee43315c
Artem Orlov <a.orlov@veedo.ru>
2020-02-25 16:28:38 +0300

    Moved protobuf generated files

--- homework/calendargrpc/gen_protobuf.sh
+++ homework/calendargrpc/gen_protobuf.sh
@@ -3,7 +3,7 @@
 cd api
 
 # Generate event interface
-protoc event.proto --go_out=plugins=grpc:../internal/domain/grpc
+protoc event.proto --go_out=plugins=grpc:../internal/grpc
 
 # Generate Calendar API server
-protoc api.proto --go_out=plugins=grpc:../internal/domain/grpc
+protoc api.proto --go_out=plugins=grpc:../internal/grpc
