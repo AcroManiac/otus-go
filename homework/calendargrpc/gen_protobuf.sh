1ae9ba9dce690cd631e69bd7bf5a6afae8a3ae2d
Artem Orlov <a.orlov@veedo.ru>
2020-02-26 19:40:33 +0300

    Moved api package to api directory

--- homework/calendargrpc/gen_protobuf.sh
+++ homework/calendargrpc/gen_protobuf.sh
@@ -3,7 +3,7 @@
 cd api
 
 # Generate event interface
-protoc event.proto --go_out=plugins=grpc:../internal/grpc
+protoc event.proto --go_out=plugins=api:../internal/api
 
 # Generate Calendar API server
-protoc api.proto --go_out=plugins=grpc:../internal/grpc
+protoc api.proto --go_out=plugins=api:../internal/api
