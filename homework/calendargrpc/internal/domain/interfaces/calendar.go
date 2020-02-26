590998043de77a44113826a033ab26956fe0babf
Artem Orlov <a.orlov@veedo.ru>
2020-02-26 19:10:26 +0300

    Added domain interfaces

---
+++ homework/calendargrpc/internal/domain/interfaces/calendar.go
@@ -0,0 +1,28 @@
+package interfaces
+
+import (
+	"time"
+
+	"github.com/AcroManiac/otus-go/homework/calendargrpc/internal/domain/entities"
+)
+
+// Calendar interfaces
+type Calendar interface {
+
+	// CreateEvent function constructs new event with default values
+	// and adds it to event storage. Returns Id of generated event or
+	// storage error if any
+	CreateEvent(startTime time.Time, stopTime time.Time) (entities.IdType, error)
+
+	// EditEvent finds event in storage by Id and replaces it
+	// with event in parameters. Returns error if event Id is wrong
+	EditEvent(id entities.IdType, ev entities.Event) error
+
+	// DeleteEvent removes event from storage by Id.
+	// Returns error if event Id is wrong
+	DeleteEvent(id entities.IdType) error
+
+	// Get event slice by time period (Day, Week, Month)
+	// Returns empty slice if there are no events
+	GetEventsByTimePeriod(period entities.TimePeriod, time time.Time) ([]entities.Event, error)
+}
---
+++ homework/calendargrpc/internal/domain/interfaces/storage.go
@@ -0,0 +1,28 @@
+package interfaces
+
+import (
+	"time"
+
+	"github.com/AcroManiac/otus-go/homework/calendargrpc/internal/domain/entities"
+)
+
+// Storage interfaces is used for event manipulation in
+// a data storage (memory, database, etc...)
+type Storage interface {
+	// Add event to data storage.
+	// The function returns event Id or error if event time is occupied
+	Add(event entities.Event) (entities.IdType, error)
+
+	// Remove event from data storage.
+	// If there is no event with time specified the function returns error
+	Remove(id entities.IdType) error
+
+	// Edit event data in data storage
+	// If event time is occupied or not in storage function returns error
+	Edit(id entities.IdType, ev entities.Event) error
+
+	// Get event slice by time period (Day, Week, Month)
+	// Function estimates start and stop of specified time period
+	// and returns events fit in it
+	GetEventsByTimePeriod(period entities.TimePeriod, time time.Time) ([]entities.Event, error)
+}
