9e4ae6d304b55ad945e78ca41d7c3c7ecd86b7fb
Artem Orlov <a.orlov@veedo.ru>
2020-02-26 19:43:45 +0300

    Fixed path to generated gRPC API

--- homework/calendargrpc/gen_protobuf.sh
+++ homework/calendargrpc/gen_protobuf.sh
@@ -3,7 +3,7 @@
 cd api
 
 # Generate event interface
-protoc event.proto --go_out=plugins=grpc:../internal/api
+protoc event.proto --go_out=plugins=grpc:../internal/infrastructure/api
 
 # Generate Calendar API server
-protoc api.proto --go_out=plugins=grpc:../internal/api
+protoc api.proto --go_out=plugins=grpc:../internal/infrastructure/api
--- homework/calendargrpc/internal/infrastructure/api/api.pb.go
+++ homework/calendargrpc/internal/infrastructure/api/api.pb.go
@@ -514,7 +514,7 @@ var _ context.Context
 var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
-// is compatible with the api package it is being compiled against.
+// is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion6
 
 // CalendarApiClient is the client API for CalendarApi service.
