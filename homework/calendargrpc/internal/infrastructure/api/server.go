979be2bc47b3e5bab5847cbc7caf9cfcfb4da913
Artem Orlov <a.orlov@veedo.ru>
2020-02-29 22:59:34 +0300

    Added create event server implementation

--- homework/calendargrpc/internal/infrastructure/api/server.go
+++ homework/calendargrpc/internal/infrastructure/api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"context"
+	"github.com/golang/protobuf/ptypes"
+	"time"
 
 	"github.com/AcroManiac/otus-go/homework/calendargrpc/internal/domain/interfaces"
 )
@@ -16,8 +18,59 @@ func NewCalendarApiServer(cal interfaces.Calendar) CalendarApiServer {
 
 func (c *CalendarApiServerImpl) CreateEvent(
 	ctx context.Context, request *CreateEventRequest) (*CreateEventResponse, error) {
-	//
-	return nil, nil
+	// Convert time from gRPC representation
+	var startTime time.Time
+	if request.GetStartTime() != nil {
+		st, err := ptypes.Timestamp(request.GetStartTime())
+		if err != nil {
+			return nil, err
+		}
+		startTime = &st
+	}
+
+	var duration time.Duration
+	if request.GetDuration() != nil {
+		d, err := ptypes.Duration(request.GetDuration())
+		if err != nil {
+			return nil, err
+		}
+		duration = &d
+	}
+
+	// Create new calendar event
+	id, err := c.cal.CreateEvent(
+		request.GetTitle(),
+		request.GetDescription(),
+		startTime,
+		duration)
+	if err != nil {
+		// Send error response
+		response := &CreateEventResponse{
+			Result: &CreateEventResponse_Error{
+				Error: err.Error(),
+			},
+		}
+		return response, err
+	}
+
+	// Create output protobuf message
+	message := &Event{
+		Id:          id,
+		Title:       request.GetTitle(),
+		Description: request.GetDescription(),
+		Owner:       "",
+		StartTime:   request.GetStartTime(),
+		Duration:    request.GetDuration(),
+		Notify:      nil,
+	}
+
+	// Send data response
+	response := &CreateEventResponse{
+		Result: &CreateEventResponse_Event{
+			Event: message,
+		},
+	}
+	return response, nil
 }
 
 func (c *CalendarApiServerImpl) EditEvent(
