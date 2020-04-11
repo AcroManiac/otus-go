d9625d01a94a621165675d1d231bd9fd1d1f6ddb
Artem Orlov <a.orlov@veedo.ru>
2020-04-11 19:56:54 +0300

    Improved requests for Calendar API

--- homework/calendargrpc/cmd/calendar_api_client/calendar_api_client.go
+++ homework/calendargrpc/cmd/calendar_api_client/calendar_api_client.go
@@ -3,8 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/AcroManiac/otus-go/homework/calendargrpc/internal/infrastructure/application"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
+
+	"github.com/AcroManiac/otus-go/homework/calendargrpc/internal/infrastructure/application"
 
 	"github.com/AcroManiac/otus-go/homework/calendargrpc/pkg/api"
 
@@ -16,15 +22,30 @@ import (
 	"github.com/spf13/viper"
 )
 
+type idsArray struct {
+	mx  *sync.Mutex
+	ids []string
+}
+
 func init() {
 	application.Init("../../configs/calendar_api_client.yaml")
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
 
+	createdEvs := &idsArray{
+		mx:  new(sync.Mutex),
+		ids: make([]string, 0, 1000),
+	}
+
 	// Create cancel context
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	// Set interrupt handler
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	// Start gRPC client
 	cc, err := grpc.Dial(
@@ -37,38 +58,114 @@ func main() {
 
 	grpcClient := api.NewCalendarApiClient(cc)
 
-	// Create and send several events to gRPC server
-	for i := 0; i < 4; i++ {
-		startTime, err := ptypes.TimestampProto(time.Now().Add(time.Minute))
-		if err != nil {
-			logger.Fatal("error converting timestamp", "error", err)
-		}
+	// Create and send create events to gRPC server
+	go func() {
+		var eventIndex int
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.NewTimer(time.Duration(rand.Intn(5)) * time.Second).C:
+				startTime, err := ptypes.TimestampProto(time.Now().Add(time.Minute))
+				if err != nil {
+					logger.Error("error converting timestamp", "error", err)
+				}
 
-		// Send create event request to gRPC server
-		createResponse, err := grpcClient.CreateEvent(ctx, &api.CreateEventRequest{
-			Title:       fmt.Sprintf("Event #%d", i),
-			Description: "Data for testing microservices",
-			Owner:       "Artem",
-			StartTime:   startTime,
-			Duration:    ptypes.DurationProto(time.Minute),
-			Notify:      ptypes.DurationProto(time.Second),
-		})
-		if err != nil {
-			logger.Error("failed sending CreateEvent request", "error", err)
-			continue
-		}
+				// Send create event request to gRPC server
+				createResponse, err := grpcClient.CreateEvent(ctx, &api.CreateEventRequest{
+					Title:       fmt.Sprintf("Event #%d", eventIndex),
+					Description: "Data for testing microservices",
+					Owner:       "Artem",
+					StartTime:   startTime,
+					Duration:    ptypes.DurationProto(time.Minute),
+					Notify:      ptypes.DurationProto(time.Second),
+				})
+				if err != nil {
+					logger.Error("failed sending CreateEvent request", "error", err)
+					continue
+				}
+				eventIndex++
 
-		// Get new event id from gRPC response
-		respEvent := createResponse.GetEvent()
-		if respEvent == nil {
-			logger.Error("response returned no event")
-			continue
-		}
-		logger.Debug("Event created in calendar")
+				// Get new event id from gRPC response
+				respEvent := createResponse.GetEvent()
+				if respEvent == nil {
+					logger.Error("response returned no event")
+					continue
+				}
+				logger.Debug("Event created in calendar")
 
-		// Wait a bit
-		time.Sleep(2 * time.Second)
-	}
+				// Save event id
+				createdEvs.mx.Lock()
+				createdEvs.ids = append(createdEvs.ids, respEvent.GetId())
+				logger.Debug(fmt.Sprintf("Array length: %d", len(createdEvs.ids)))
+				createdEvs.mx.Unlock()
+			}
+		}
+	}()
+
+	// Get events from gRPC server
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.NewTimer(time.Duration(rand.Intn(3)) * time.Minute).C:
+				searchTime, err := ptypes.TimestampProto(time.Now())
+				if err != nil {
+					logger.Error("error converting timestamp", "error", err)
+				}
+				searchResponse, err := grpcClient.GetEvents(ctx, &api.GetEventsRequest{
+					Period:    api.TimePeriod_TIME_DAY,
+					StartTime: searchTime,
+				})
+				if err != nil {
+					logger.Error("failed sending GetEvents request", "error", err)
+				}
+
+				// Get events from response
+				events := searchResponse.GetEvents()
+				if events == nil {
+					logger.Info("response returned no event")
+				}
+				logger.Info(fmt.Sprintf("GetEvents returned %d events", len(events)))
+			}
+		}
+	}()
+
+	// Delete events from calendar
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.NewTimer(time.Duration(2) * time.Minute).C:
+
+				// Copy ids and clear array
+				createdEvs.mx.Lock()
+				copiedIds := make([]string, len(createdEvs.ids))
+				copy(copiedIds, createdEvs.ids)
+				createdEvs.ids = make([]string, 0, 1000)
+				createdEvs.mx.Unlock()
+
+				logger.Info(fmt.Sprintf("Deleting %d events", len(copiedIds)))
+				for _, id := range copiedIds {
+					// Delete event through gRPC request
+					_, err = grpcClient.DeleteEvent(ctx, &api.DeleteEventRequest{
+						Id: id,
+					})
+					if err != nil {
+						logger.Error("failed sending DeleteEvent request", "error", err)
+					}
+				}
+			}
+		}
+	}()
+
+	// Wait for user or OS interrupt
+	<-done
+
+	// Call context to stop i/o operations
+	cancel()
 
 	logger.Info("Client exited")
 }
